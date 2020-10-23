7d39243c260abf69ed61528232bfc4528b1c3aac
Alan Jeremias Perez <aaperez.tomas@gmail.com>
2020-10-22 21:40:41 -0600

    Componentes para compra de giftcard

---
+++ Frontend/client/src/app/components/compra-giftcard/compra-giftcard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compra-giftcard',
+  templateUrl: './compra-giftcard.component.html',
+  styleUrls: ['./compra-giftcard.component.css']
+})
+export class CompraGiftcardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
