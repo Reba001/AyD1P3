747553ce08fee56484375a52e8bc280af1a7e8e1
Alejandro <alerod620@gmail.com>
2020-10-22 23:55:23 -0600

    Registro de compra

--- Backend/Server/src/events.js
+++ Backend/Server/src/events.js
@@ -221,6 +221,25 @@ router.get('/perfil_usuario/:id', (req, res, next) => {
   );
 });
 
+//******Factura******
+router.post('/registrar_compra', (req, res, next) => {
+  db.query(
+    'INSERT INTO FACTURA(fecha, estado, no_tarjeta, id_usuario)',
+    [req.body.fecha, req.body.estado, req.body.tarjeta, req.body.id],
+    (error, results) => {
+      if(error)
+      {
+        console.error(error);
+        res.status(500).json({status:'error'});
+      }
+      else
+      {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
   return router;
 }
 module.exports = createRouter;
