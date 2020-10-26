9c5f1217fcf58d005e16f0bbc4bd627c3ddde30d
OscarJO <oscar.jordan.21091@gmail.com>
2020-10-26 14:50:01 -0600

    creación de modelo de datos de perfil

---
+++ Frontend/client/src/app/models/usuarioperfil.spec.ts
@@ -0,0 +1,7 @@
+import { Usuarioperfil } from './usuarioperfil';
+
+describe('Usuarioperfil', () => {
+  it('should create an instance', () => {
+    expect(new Usuarioperfil()).toBeTruthy();
+  });
+});
---
+++ Frontend/client/src/app/models/usuarioperfil.ts
@@ -0,0 +1,11 @@
+export class Usuarioperfil {
+    
+        nombre:string;
+        apellido:string;
+        edad:number;
+        dpi:string;
+        username:string;
+        correo:string;
+        password:string;
+    
+}
