e4a909017d8e2d049460611403535d99d1bf7948
OscarJO <oscar.jordan.21091@gmail.com>
2020-10-27 08:34:28 -0600

    primeras pruebas unitarias

--- Frontend/client/src/app/components/perfil/perfil.component.spec.ts
+++ Frontend/client/src/app/components/perfil/perfil.component.spec.ts
@@ -1,10 +1,13 @@
+import { stringify } from '@angular/compiler/src/util';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PerfilComponent } from './perfil.component';
+
+
 
 describe('PerfilComponent', () => {
   let component: PerfilComponent;
   let fixture: ComponentFixture<PerfilComponent>;
+  let username:string;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,4 +25,9 @@ describe('PerfilComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('username no debería ser vacio', () => {
+    expect(username).toEqual(jasmine.any(String));
+  });
+
 });
