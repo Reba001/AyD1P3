1fc2e416f00e88ede22e0961e908bc0aa4d5a812
Alejandro <alerod620@gmail.com>
2020-10-28 13:18:10 -0600

    Creación método para devolver el historial de facturas

--- Backend/Server/src/events.js
+++ Backend/Server/src/events.js
@@ -410,6 +410,36 @@ router.get('/api_tasa', tasaController.listTasas);
 //Valores
 router.get('/api_value', valueController.listValues);
 
+
+//******Obtener informacion de usuario ******
+router.get('/historial_factura/:id', (req, res, next) => {
+  const {id} = req.params;
+  db.query(
+    'SELECT f.fecha, f.estado, f.no_tarjeta, g.nombre AS Tarjeta, (d.cantidad * d.precio) AS Total\
+    FROM USUARIO u\
+    JOIN FACTURA f USING(id_usuario)\
+    JOIN DETALLE d USING(id_factura)\
+    JOIN GIFTCARDS g USING(id_giftcards)\
+    WHERE id_usuario = ?',
+    [id],
+    (error, results) => {
+      if(error)
+      {
+        console.error(error);
+        res.status(500).json({status:'error'});
+      }
+      else
+      {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+
+
+
+
   return router;
 }
 
